132dcb16630478adef3ba8a266263bb68ae5862f
Kyle Waz <138908+wazhere@users.noreply.github.com>
2025-02-04 13:00:39 -0500

    Update

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,15 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page >
+    
+<div class="flex flex-center" style="margin-top: 100px;">
     <q-btn icon="check_circle" color="primary" label="Open Modal No Layout" @click="showDialogNoLayout = true" />&nbsp;
     <q-btn icon="error" color="primary" label="Open Modal Layout Container" @click="showDialogLayoutContainer = true" />&nbsp;
     <q-btn icon="check_circle" color="primary" label="Open Modal Layout No Container" @click="showDialogLayoutNoContainer = true" />&nbsp;
     <lazy-true-to-form-dialog-no-layout v-model="showDialogNoLayout" v-if="showDialogNoLayout" />
     <lazy-true-to-form-dialog-layout-container v-model="showDialogLayoutContainer" v-if="showDialogLayoutContainer" />
     <lazy-true-to-form-dialog-layout-no-container v-model="showDialogLayoutNoContainer" v-if="showDialogLayoutNoContainer" />
-  </q-page>
+  </div>
+</q-page>
 </template>
 
 <script>
