b6e635195eb4fdd4e5132d42e6e650ac89047e80
Kyle Waz <138908+wazhere@users.noreply.github.com>
2025-02-04 12:38:12 -0500

    Update

--- src/components/TrueToFormDialogLayoutContainer.vue
+++ src/components/TrueToFormDialogLayoutContainer.vue
@@ -17,8 +17,8 @@
         <div style="background-color: red; color: white">
           TTF_WIDGET_CONTAINER
         </div>
+        <div style="border: solid 1px red; height: 40px; display: inline-block;">            
           <div class="row justify-center">
-            <div style="border: solid 1px red; height: 40px; display: inline-block;">
             <div
                 id="TTF_WIDGET_CONTAINER"
                 data-product-id="TH134"
