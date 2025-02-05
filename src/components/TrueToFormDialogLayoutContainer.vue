4eda706cd43d81ac68377bdddadfb9f4ec4df942
Mostafa Hesham <m.hesham3001@gmail.com>
2025-02-06 00:22:01 +0200

    style: make the layout take the full width/height

--- src/components/TrueToFormDialogLayoutContainer.vue
+++ src/components/TrueToFormDialogLayoutContainer.vue
@@ -3,39 +3,42 @@
     :model-value="modelValue"
     @update:model-value="$emit('update:modelValue', $event)"
     @before-show="InitializeWidget"
+    :maximized="true"
   >
     <q-layout ref="TabRef" container class="bg-lm-lightest">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">TTF Example TH134 Modal</div>
-        </q-card-section>
+      <div class="flex justify-center items-center h-full">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6">TTF Example TH134 Modal</div>
+          </q-card-section>
 
-        <q-card-section>
-          <div style="background-color: red; color: white">TTF_WIDGET_CONTAINER</div>
-          <div style="border: solid 1px red; height: 40px">
-            <div class="row justify-center" style="padding-top: 10px">
-              <div
-                id="TTF_WIDGET_CONTAINER"
-                data-api-key="Ca42eYkVLSEXLdhJhjq2"
-                data-product-id="9522490933534"
-              ></div>
+          <q-card-section>
+            <div style="background-color: red; color: white">TTF_WIDGET_CONTAINER</div>
+            <div style="border: solid 1px red; height: 40px">
+              <div class="row justify-center" style="padding-top: 10px">
+                <div
+                  id="TTF_WIDGET_CONTAINER"
+                  data-api-key="Ca42eYkVLSEXLdhJhjq2"
+                  data-product-id="9522490933534"
+                ></div>
+              </div>
             </div>
-          </div>
-        </q-card-section>
+          </q-card-section>
 
-        <q-card-section>
-          <div
-            style="min-width: 500px; text-align: center; padding: 30px 0; background-color: #eee"
-          >
-            EXISTING TH134 CHOOSE SIZE STUFF
-          </div>
-        </q-card-section>
+          <q-card-section>
+            <div
+              style="min-width: 500px; text-align: center; padding: 30px 0; background-color: #eee"
+            >
+              EXISTING TH134 CHOOSE SIZE STUFF
+            </div>
+          </q-card-section>
 
-        <q-card-actions align="right">
-          <q-btn flat label="Unload TTF Script" @click="UnloadWidgetScript()" />
-          <q-btn flat label="Close" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
+          <q-card-actions align="right">
+            <q-btn flat label="Unload TTF Script" @click="UnloadWidgetScript()" />
+            <q-btn flat label="Close" color="primary" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </div>
     </q-layout>
   </q-dialog>
 </template>
